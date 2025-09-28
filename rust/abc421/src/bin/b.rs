20a5b3bedb7dcb09c9d0304ecf7318caea9df7af
74keh <74keh.dev@gmail.com>
2025-09-28 16:15:57 +0900

    add abc421-b

--- rust/abc421/src/bin/b.rs
+++ rust/abc421/src/bin/b.rs
@@ -2,6 +2,20 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        mut x: usize,
+        mut y: usize,
     }
+    for _ in 3..=10 {
+        (x, y) = (y, f(x + y));
+    }
+    println!("{}", y);
+}
+
+fn f(mut x: usize) -> usize {
+    let mut result = 0;
+    while x > 0 {
+        result = result * 10 + x % 10;
+        x /= 10;
+    }
+    result
 }
