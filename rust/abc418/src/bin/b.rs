f61053ba5720299ab8d478049a6d4a8a4e963929
74keh <74keh.dev@gmail.com>
2025-10-11 18:51:44 +0900

    add abc418-b

--- rust/abc418/src/bin/b.rs
+++ rust/abc418/src/bin/b.rs
@@ -1,7 +1,24 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-
+        s: Chars,
     }
+    let n = s.len();
+    let mut result = 0.0_f64;
+    if n > 2 {
+        for i in 0..(n - 1) {
+            if s[i] != 't' {
+                continue;
+            }
+            let mut count = 1;
+            for j in (i + 1)..n {
+                if s[j] == 't' {
+                    count += 1;
+                    result = result.max((count - 2) as f64 / (j - i - 1) as f64);
+                }
+            }
+        }
+    }
+    println!("{}", result);
 }
