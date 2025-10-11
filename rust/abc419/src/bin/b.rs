ab883c5426a5ac18a178797b6b26ea23c252fc50
74keh <74keh.dev@gmail.com>
2025-10-11 18:19:51 +0900

    add abc419-b

--- rust/abc419/src/bin/b.rs
+++ rust/abc419/src/bin/b.rs
@@ -1,7 +1,30 @@
+use std::{cmp::Reverse, collections::BinaryHeap};
+
 use proconio::input;
 
 fn main() {
     input! {
-
+        q: usize,
+    }
+    let mut queue = BinaryHeap::new();
+    for _ in 0..q {
+        input! {
+            t: u8,
+        }
+        match t {
+            1 => {
+                input! {
+                    x: usize,
+                }
+                queue.push(Reverse(x));
+            }
+            2 => {
+                let Some(Reverse(result)) = queue.pop() else {
+                    unreachable!()
+                };
+                println!("{}", result);
+            }
+            _ => unreachable!(),
+        }
     }
 }
