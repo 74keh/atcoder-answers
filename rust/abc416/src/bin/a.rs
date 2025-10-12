a073497f526eea84aca71bcc04b626e47d145a46
74keh <74keh.dev@gmail.com>
2025-10-12 22:12:48 +0900

    add abc416-a

--- rust/abc416/src/bin/a.rs
+++ rust/abc416/src/bin/a.rs
@@ -1,7 +1,12 @@
-use proconio::input;
+use proconio::{input, marker::Usize1};
 
 fn main() {
     input! {
-
+        _n: usize,
+        l: Usize1,
+        r: Usize1,
+        s: String,
     }
+    let yes = !s[l..=r].contains('x');
+    println!("{}", if yes { "Yes" } else { "No" });
 }
