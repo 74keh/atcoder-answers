953a2de8f27cf909a50bee5050d29f45b99da867
74keh <74keh.dev@gmail.com>
2025-10-12 23:27:36 +0900

    add abc414-a

--- rust/abc414/src/bin/a.rs
+++ rust/abc414/src/bin/a.rs
@@ -2,6 +2,19 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        l: u8,
+        r: u8,
     }
+    let mut count = 0;
+    for _ in 0..n {
+        input! {
+            x: u8,
+            y: u8,
+        }
+        if x <= l && r <= y {
+            count += 1;
+        }
+    }
+    println!("{}", count);
 }
