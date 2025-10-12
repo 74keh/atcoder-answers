86dc2a2590aee388e43c376d654e238419f7b1f6
74keh <74keh.dev@gmail.com>
2025-10-12 22:35:37 +0900

    add abc415-a

--- rust/abc415/src/bin/a.rs
+++ rust/abc415/src/bin/a.rs
@@ -2,6 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        a: [usize; n],
+        x: usize,
+    }
+    if a.iter().any(|&a| a == x) {
+        println!("Yes");
+    } else {
+        println!("No");
     }
 }
