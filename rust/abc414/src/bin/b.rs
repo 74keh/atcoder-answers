9156f5d9327c880a07ff45083af3c1d158f94876
74keh <74keh.dev@gmail.com>
2025-10-12 23:27:51 +0900

    add abc414-b

--- rust/abc414/src/bin/b.rs
+++ rust/abc414/src/bin/b.rs
@@ -2,6 +2,18 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        cl: [(char, usize); n]
     }
+    let mut result = String::new();
+    for (c, l) in cl {
+        if result.len() + l > 100 {
+            println!("Too Long");
+            return;
+        }
+        for _ in 0..l {
+            result.push(c);
+        }
+    }
+    println!("{}", result);
 }
