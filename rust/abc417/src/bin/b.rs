49e80f0dd02195c565a137bd125d1fd95abd7827
74keh <74keh.dev@gmail.com>
2025-10-12 10:11:36 +0900

    add abc417-b

--- rust/abc417/src/bin/b.rs
+++ rust/abc417/src/bin/b.rs
@@ -1,7 +1,22 @@
+use itertools::Itertools;
 use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        m: usize,
+        mut a: [usize; n],
+        b: [usize; m],
+    }
+    for b in b {
+        for i in 0..a.len() {
+            if a[i] == b {
+                a.remove(i);
+                break;
+            }
+        }
+    }
+    if !a.is_empty() {
+        println!("{}", a.iter().join(" "));
     }
 }
