676a13c6bbc24b7943c4ec7d8a4fd8297fbc9be8
74keh <74keh.dev@gmail.com>
2025-10-12 22:13:42 +0900

    add abc416-b

--- rust/abc416/src/bin/b.rs
+++ rust/abc416/src/bin/b.rs
@@ -1,7 +1,19 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-
+        s: Chars,
     }
+    for i in 0..s.len() {
+        if s[i] == '#' {
+            print!("#");
+        }
+        else if i == 0 || s[i-1] == '#' {
+            print!("o");
+        }
+        else {
+            print!(".");
+        }
+    }
+    println!("");
 }
