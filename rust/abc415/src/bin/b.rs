1811d6bcdabdf9d0b6082da4d7f50eebe20c7b35
74keh <74keh.dev@gmail.com>
2025-10-12 22:35:46 +0900

    add abc415-b

--- rust/abc415/src/bin/b.rs
+++ rust/abc415/src/bin/b.rs
@@ -1,7 +1,16 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-
+        s: Chars,
+    }
+    let mut a = vec![];
+    for i in 0..s.len() {
+        if s[i] == '#' {
+            a.push(i+1);
+        }
+    }
+    for i in (0..a.len()).step_by(2) {
+        println!("{},{}", a[i], a[i+1]);
     }
 }
