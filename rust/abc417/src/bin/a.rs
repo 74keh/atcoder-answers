04ebe5e8d949706cb873a20bae0f6a95ed3edf51
74keh <74keh.dev@gmail.com>
2025-10-12 10:11:25 +0900

    add abc417-a

--- rust/abc417/src/bin/a.rs
+++ rust/abc417/src/bin/a.rs
@@ -1,7 +1,12 @@
-use proconio::input;
+use itertools::Itertools;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-
+        n: usize,
+        a: usize,
+        b: usize,
+        s: Chars,
     }
+    println!("{}", &s[a..n - b].iter().join(""));
 }
