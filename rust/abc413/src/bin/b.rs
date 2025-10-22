06156d480a641576ff73b9884174880cc5e28929
74keh <74keh.dev@gmail.com>
2025-10-22 22:52:28 +0900

    add abc413-b

--- rust/abc413/src/bin/b.rs
+++ rust/abc413/src/bin/b.rs
@@ -1,7 +1,19 @@
+use std::collections::HashSet;
 use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        s: [String; n],
     }
+    let mut set = HashSet::new();
+    for i in 0..n {
+        for j in 0..n {
+            if i == j {
+                continue;
+            }
+            set.insert(format!("{}{}", s[i], s[j]));
+        }
+    }
+    println!("{}", set.len());
 }
