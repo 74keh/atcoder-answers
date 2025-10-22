fb39e2c1eb97f57870d15393bcd76d098b83dca1
74keh <74keh.dev@gmail.com>
2025-10-22 21:52:31 +0900

    add abc413-a

--- rust/abc413/src/bin/a.rs
+++ rust/abc413/src/bin/a.rs
@@ -2,6 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        m: usize,
+        a: [usize; n],
+    }
+    let sum: usize = a.iter().sum();
+    if sum <= m {
+        println!("Yes");
+    } else {
+        println!("No");
     }
 }
