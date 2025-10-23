50c3e4bc5f4a32bc3b53924a84ca6c29ddf918d8
74keh <74keh.dev@gmail.com>
2025-10-23 09:49:31 +0900

    add abc412-b

--- rust/abc412/src/bin/b.rs
+++ rust/abc412/src/bin/b.rs
@@ -1,7 +1,15 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-
+        s: Chars,
+        t: Chars,
     }
+    let mut yes = true;
+    for i in 1..s.len() {
+        if s[i].is_uppercase() {
+            yes &= t.contains(&s[i - 1]);
+        }
+    }
+    println!("{}", if yes {"Yes"} else {"No"});
 }
