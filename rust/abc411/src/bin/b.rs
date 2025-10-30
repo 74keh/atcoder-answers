72db3775e1705c3ca605d0628758d4893427ff88
74keh <74keh.dev@gmail.com>
2025-10-30 09:10:40 +0900

    add abc411-b

--- rust/abc411/src/bin/b.rs
+++ rust/abc411/src/bin/b.rs
@@ -2,6 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        d: [usize; n-1],
+    }
+    for i in 0..n-1 {
+        let mut a = d[i];
+        print!("{}", a);
+        for j in i+1..n-1 {
+            a += d[j];
+            print!(" {}", a);
+        }
+        println!();
     }
 }
