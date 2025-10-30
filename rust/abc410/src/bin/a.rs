4fe80093938d991b0e29f4324c10735e58764b90
74keh <74keh.dev@gmail.com>
2025-10-30 09:21:52 +0900

    add abc410-a

--- rust/abc410/src/bin/a.rs
+++ rust/abc410/src/bin/a.rs
@@ -2,6 +2,10 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        a: [usize; n],
+        k: usize,
     }
+    let result = a.iter().filter(|&&x| k <= x).count();
+    println!("{result}");
 }
