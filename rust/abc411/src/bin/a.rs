89f0293c0a14ee0a0f2edaa26a8b11d4deab3d9c
74keh <74keh.dev@gmail.com>
2025-10-30 09:10:14 +0900

    add abc411-a

--- rust/abc411/src/bin/a.rs
+++ rust/abc411/src/bin/a.rs
@@ -1,7 +1,13 @@
-use proconio::input;
+use proconio::{input, marker::Chars};
 
 fn main() {
     input! {
-
+        p: Chars,
+        l: usize,
+    }
+    if p.len() >= l {
+        println!("Yes");
+    } else {
+        println!("No");
     }
 }
