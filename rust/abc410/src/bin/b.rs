dc5e1215db24f9c52da71291950aa26c2b69bfc1
74keh <74keh.dev@gmail.com>
2025-11-02 20:07:37 +0900

    add abc410-a

--- rust/abc410/src/bin/b.rs
+++ rust/abc410/src/bin/b.rs
@@ -2,6 +2,28 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        q: usize,
+        x: [usize; q],
     }
+    let mut b = vec![0; n+1];
+    for i in 0..q {
+        if i > 0 {
+            print!(" ");
+        }
+        if x[i] >= 1 {
+            b[x[i]] += 1;
+            print!("{}", x[i]);
+        } else {
+            let mut y = 1;
+            for j in 2..=n {
+                if b[y] > b[j] {
+                    y = j;
+                }
+            }
+            b[y] += 1;
+            print!("{}", y);
+        }
+    }
+    println!();
 }
