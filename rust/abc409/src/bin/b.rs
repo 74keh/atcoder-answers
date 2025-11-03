2725c222805e3d6cc9517475b42f903cefa857f8
74keh <74keh.dev@gmail.com>
2025-11-03 13:37:23 +0900

    add abc409-b

--- rust/abc409/src/bin/b.rs
+++ rust/abc409/src/bin/b.rs
@@ -2,6 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        mut a: [usize; n],
     }
+    a.sort_unstable();
+    let mut ans = 0;
+    for i in 0..n {
+        ans = ans.max(a[i].min(a.len() - i));
+        
+    }
+    println!("{}", ans);
 }
