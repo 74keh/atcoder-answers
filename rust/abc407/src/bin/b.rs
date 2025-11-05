b0ce1b6365220dfa0abc7b1c90ce151d59da9bf2
74keh <74keh.dev@gmail.com>
2025-11-06 00:25:24 +0900

    add abc407-b

--- rust/abc407/src/bin/b.rs
+++ rust/abc407/src/bin/b.rs
@@ -2,6 +2,16 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        x: u32,
+        y: u32,
     }
+    let mut count = 0;
+    for i in 1..=6 {
+        for j in 1..=6 {
+            if i + j >= x || i.abs_diff(j) >= y {
+                count += 1;
+            }
+        }
+    }
+    println!("{}", count as f64 / 36.0);
 }
