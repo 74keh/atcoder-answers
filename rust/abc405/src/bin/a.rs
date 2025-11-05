59224f54362f62b2b2c2a60674246c62ba4815bf
74keh <74keh.dev@gmail.com>
2025-11-06 00:49:53 +0900

    add abc405-a

--- rust/abc405/src/bin/a.rs
+++ rust/abc405/src/bin/a.rs
@@ -2,6 +2,13 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        r: u32,
+        x: u32
     }
+    let rated = if x == 1 {
+        1600 <= r && r <= 2999
+    } else {
+        1200 <= r && r <= 2399
+    };
+    println!("{}", if rated {"Yes"} else {"No"});
 }
