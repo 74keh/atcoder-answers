a0e9f58b69aad2c153470734e3cb56bc98daca13
74keh <74keh.dev@gmail.com>
2025-11-08 16:43:29 +0900

    add abc404-a

--- rust/abc404/src/bin/a.rs
+++ rust/abc404/src/bin/a.rs
@@ -2,6 +2,11 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        s: String,
+    }
+    if let Some(result) = ('a'..='z').find(|&c| !s.contains(c)) {
+        println!("{}", result);
+    } else {
+        println!("None");
     }
 }
