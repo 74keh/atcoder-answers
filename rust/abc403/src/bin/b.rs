244c217a1ba4dd901dfafcc39f1ca2ebfbd3fc23
74keh <74keh.dev@gmail.com>
2025-11-29 21:40:32 +0900

    add abc403-b

--- rust/abc403/src/bin/b.rs
+++ rust/abc403/src/bin/b.rs
@@ -1,7 +1,28 @@
+use proconio::marker::Chars;
 use proconio::input;
 
 fn main() {
     input! {
+        t: Chars,
+        u: Chars,
+    }
+
+    for i in 0..=t.len() - u.len() {
+        let mut ok = true;
+
+        for j in 0..u.len() {
+            if t[i + j] != u[j] && t[i + j] != '?' {
+                ok = false;
+                break;
+            }
+        }
+
+        if ok {
+            println!("Yes");
+            return;
+        }
 
     }
+
+    println!("No");
 }
