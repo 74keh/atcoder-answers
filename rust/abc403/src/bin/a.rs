b86e4701c2b42ff9d48eca6215ef6b5147c6506a
74keh <74keh.dev@gmail.com>
2025-11-29 21:19:35 +0900

    add abc403-a

--- rust/abc403/src/bin/a.rs
+++ rust/abc403/src/bin/a.rs
@@ -2,6 +2,14 @@ use proconio::input;
 
 fn main() {
     input! {
-
+        n: usize,
+        a: [usize; n],
     }
+    let mut sum = 0;
+    for i in 0..n {
+        if i % 2 == 0 {
+            sum += a[i];
+        }
+    }
+    println!("{}", sum);
 }
