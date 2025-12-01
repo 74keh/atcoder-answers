6c6cbe74967e130572cca353a5c38a5621265fbd
74keh <74keh.dev@gmail.com>
2025-12-01 23:56:56 +0900

    add abc405-b

--- rust/abc405/src/bin/b.rs
+++ rust/abc405/src/bin/b.rs
@@ -2,6 +2,22 @@ use proconio::input;
 
 fn main() {
     input! {
+        n: usize,
+        m: usize,
+        mut a: [usize; n],
+    }
 
+    loop {
+        let mut f = vec![false; m];
+        for &a in &a {
+            f[a] = true;
+        }
+
+        if let Some(i) = (1..=m).find(|&i| !f[i]) {
+            println!("{}", i);
+            return;
+        }
+        
+        a.pop();
     }
 }
